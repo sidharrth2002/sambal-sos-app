73602c9a46755b00950b75480ca18b5b5c936fbd
Shaunmak1214 <shaonmak@gmail.com>
2021-07-01 12:35:49 +0800

    feat: firdt try tailwind css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-lg" >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
