e70668094bfa518b18d5ec61d7f4f63f3d0b4552
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-07-13 15:32:14 +0800

    all fields dynamically translated

--- src/screens/ReportForm/index.js
+++ src/screens/ReportForm/index.js
@@ -18,12 +18,14 @@ import * as BDGraphics from '../../assets/';
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next';
 require('dotenv').config()
 
 const ReportForm = () => {
     const accessToken = useSelector(state => state.auth.accessToken);
     const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
     const toast = useToast()
+    const { t } = useTranslation();
 
     /* States */
     const [remark, setRemark] = useState('');
@@ -220,10 +222,11 @@ const ReportForm = () => {
                 <Box className="navbar" backgroundColor="#FFFFFF" px="2.0rem" py="1.0rem" >
                     <Flex position="relative" flexDirection="row" justifyContent="center" alignItems="center" >
                         <Box onClick={ () => {window.location = '/home'} } className="back-button" position="absolute" left="5px" >
-                            <Image h="25px" src={ BDGraphics.BackButtonIcon } alt="back button" />
+                            {/* <Image h="25px" src={ BDGraphics.BackButtonIcon } alt="back button" /> */}
+                            <></>
                         </Box>
                         <Box className="screen-title" >
-                            <Text fontFamily="Poppins" fontWeight="600" >REPORT FOR HELP</Text>
+                            <Text fontFamily="Poppins" fontWeight="600" >{t("report-form.report-for-help")}</Text>
                         </Box>
                         <Box className="info-button" position="absolute" right="5px">
                             <Image h="18px" src={ BDGraphics.InfoIcon } alt="back button" />
@@ -245,32 +248,32 @@ const ReportForm = () => {
                                         acceptedFiles.length > 0 ?
                                         <Alert status="success">
                                             <AlertIcon />
-                                            Image Added! 😄
+                                            {t("report-form.image-added")}
                                         </Alert>
                                         :
                                         <Center w="100%" padding="40px 40px" borderRadius="8px" border="1px solid #DEDEDE"  >
                                             <Center flexDirection="column" {...getRootProps({className: 'dropzone'})}>
                                                 <Image mb="20px" src={BDGraphics.ImageIcon} alt="Image" />
                                                 <input {...getInputProps()} required />
-                                                <Text color="#A7A7A7" fontSize="md">Add an image (We want to protect privacy. Please just take a picture in front of the house.) <Text fontWeight="bold">There should not be a white flag in the image.</Text></Text>
+                                                <Text color="#A7A7A7" fontSize="md">{t("report-form.add-image-prompt")}<Text fontWeight="bold">{t("report-form.should-not-be-a-white-flag")}</Text></Text>
                                             </Center>
                                         </Center>
                                     }
-                                    <Text mt="10px" color="#A7A7A7" textAlign="start" px="0.5rem">By uploading this image, you understand that the image will be public. Please ensure that there is no privacy breach. Your picture will be reviewed.</Text>
+                                    <Text mt="10px" color="#A7A7A7" textAlign="start" px="0.5rem">{t("report-form.upload-disclaimer")}</Text>
                             </Flex>
                         </Flex>
 
                         <Flex className="Form-Blocks" flexDirection="column" alignItems="flex-start" mb="4rem" >
                             <Flex className="Form-Title" flexDirection="row" alignItems="center" mb="15px" >
                                 <Text fontSize="xs" fontFamily="Poppins" fontWeight="500" color="#6598FF" mr="10px" >Step 2 of 4</Text>
-                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" >Set the address</Text>
+                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" >{t("report-form.set-address")}</Text>
                             </Flex>
                             <Flex className="Form-Content" w="100%" flexDirection="column" justifyContent="center" alignItems="center" >
                                 {
                                     coordinates ?
                                     <Alert status="success" mb="15px">
                                         <AlertIcon />
-                                        Location has been set! 😄
+                                        {t("report-form.location-set")}
                                     </Alert>
                                     :
                                     <Flex display={useMyLocationEnable ? 'block' : 'none'} flexDirection="row" justifyContent="center" alignItems="center" w="100%" h="100%" backgroundColor="#464646" borderRadius="5px" color="white" padding="1.0rem" onClick={() => popupLocation()}>
@@ -278,7 +281,7 @@ const ReportForm = () => {
                                             !locationLoading ?
                                                 <Flex flexDirection="row" justifyContent="center" alignItems="center" >
                                                     <Image h="15px" w="15px" mr="0.9rem" src={ BDGraphics.LocationIcon } alt="Location Icon" />
-                                                    Use My Location
+                                                    {t("report-form.use-location-button")}
                                                 </Flex>
                                             :
                                             <Spinner />
@@ -287,7 +290,7 @@ const ReportForm = () => {
 
                                 }
 
-                                <Text display={(useMyLocationEnable && searchLocationEnable) ? 'block' : 'none'} marginY="10px" fontFamily="Poppins" fontSize="13px" color="#5F5F5F" > Or </Text>
+                                <Text display={(useMyLocationEnable && searchLocationEnable) ? 'block' : 'none'} marginY="10px" fontFamily="Poppins" fontSize="13px" color="#5F5F5F" > {t("or")} </Text>
 
                                 <Box display={searchLocationEnable ? 'block' : 'none'} w="100%" ref={ref}>
                                     <Input
@@ -296,7 +299,7 @@ const ReportForm = () => {
                                         value={value}
                                         onChange={handleInput}
                                         disabled={!ready}
-                                        placeholder="Where are you?"
+                                        placeholder={t("report-form.where-are-you")}
                                     />
                                     {status === "OK" && <Box backgroundColor="#FFFFFF" w="100%" padding="1rem" border="1px solid #F5F5F5" mt="15px" borderRadius="8px" boxShadow="0px 16px 40px rgba(212, 212, 212, 0.25);" >{renderSuggestions()}</Box>}
                                 </Box>
@@ -306,7 +309,7 @@ const ReportForm = () => {
                         <Flex className="Form-Blocks" flexDirection="column" alignItems="flex-start" mb="3rem" >
                             <Flex className="Form-Title" flexDirection="row" alignItems="center" mb="15px" >
                                 <Text fontSize="xs" fontFamily="Poppins" fontWeight="500" color="#6598FF" mr="10px" >Step 3 of 4</Text>
-                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" >Remarks</Text>
+                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" >{t("report-form.remarks")}</Text>
                             </Flex>
                             <Flex className="Form-Content" w="100%" flexDirection="column" justifyContent="center" alignItems="center" >
                                 <Textarea placeholder="Eg: What did you see? What do you need?" onChange={ handleRemark } />
@@ -316,45 +319,44 @@ const ReportForm = () => {
                         <Flex className="Form-Blocks" flexDirection="column" alignItems="flex-start" mb="3rem" >
                             <Flex className="Form-Title" flexDirection="row" alignItems="center" mb="15px" >
                                 <Text fontSize="xs" fontFamily="Poppins" fontWeight="500" color="#6598FF" mr="10px" >Step 4 of 4</Text>
-                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" mr="5px" >Phone Number</Text>
-                                <Text fontSize="xs" fontFamily="Poppins" fontWeight="400">(Optional)</Text>
+                                <Text fontSize="lg" fontFamily="Poppins" fontWeight="500" mr="5px" >{t("report-form.what-did-you-see")}</Text>
+                                <Text fontSize="xs" fontFamily="Poppins" fontWeight="400">({t("report-form.optional")})</Text>
                             </Flex>
                             <Flex className="Form-Content" w="100%" flexDirection="column" justifyContent="center" alignItems="center" >
                                 <InputGroup>
                                     <InputLeftAddon children="+60" />
-                                    <Input isInvalid={ phoneNumberError ? true : false } focusBorderColor={ phoneNumberError ? 'red.300' : '' } type="tel" placeholder="Enter your phone number" onChange={ handlePhoneNumber } />
+                                    <Input isInvalid={ phoneNumberError ? true : false } focusBorderColor={ phoneNumberError ? 'red.300' : '' } type="tel" placeholder={t("report-form.enter-phone-number")} onChange={ handlePhoneNumber } />
                                 </InputGroup>
                             </Flex>
                         </Flex>
 
                         <Box textAlign="start" mb="1rem">
                             <Text fontWeight="medium" mb="20px">
-                                In order to maintain a healthy environment on the Sambal SOS App, all reports made will be subject to human review.
-                                This is to ensure that the reports made on the Sambal SOS App are authentic, and respecting. Reports are typically approved/rejected within 24 hours of posting.
+                                {t("report-form.human-review-disclaimer")}
                             </Text>
 
                             <Text fontWeight="light" mb="20px" >
-                                <Text mb="10px" fontWeight="medium" >If you're creating an SOS report on the Sambal SOS App, do follow the following guidelines: </Text>
+                                <Text mb="10px" fontWeight="medium" >{t("report-form.terms-title")}</Text>
                                 <List spacing={3}>
                                     <ListItem>
                                         <ListIcon as={CheckIcon} color="green.500" />
-                                        Images could be, but not limited to: location of the report, screenshots of chat histories
+                                        {t("report-form.term-1")}
                                     </ListItem>
                                     <ListItem>
                                         <ListIcon as={CheckIcon} color="green.500" />
-                                        Images should not contain faces of people, or personal identifiable information
+                                        {t("report-form.term-2")}
                                     </ListItem>
                                     <ListItem>
                                         <ListIcon as={CheckIcon} color="green.500" />
-                                        Content in the SOS report should provide more details of the report so that other users of the app are able to discover the rough location of those who are in need
+                                        {t("report-form.term-3")}
                                     </ListItem>
                                     <ListItem>
                                         <ListIcon as={CheckIcon} color="green.500" />
-                                        Strictly no NSFW content, political content, sensitive issues, religious issues, or any other content that violates Article 10 of the Constitution of Malaysia
+                                        {t("report-form.term-4")}
                                     </ListItem>
                                     <ListItem>
                                         <Box marginBottom="1rem">
-                                            By submitting the form, you agree to our <Link href="/privacy-policy" target="_blank" color="blue">privacy policy</Link>.
+                                            {t("report-form.term-5")} <Link href="/privacy-policy" target="_blank" color="blue">{t("report-form.privacy-policy")}</Link>.
                                         </Box>
                                     </ListItem>
                                 </List>
@@ -366,7 +368,7 @@ const ReportForm = () => {
                                 submitLoading ?
                                 <Spinner />
                                 :
-                                <Text color="white" fontFamily="Montserrat" fontWeight="800" >Submit</Text>
+                                <Text color="white" fontFamily="Montserrat" fontWeight="800" >{t("report-form.submit-button")}</Text>
                             }
                         </Center>
 
