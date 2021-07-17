c1fc9d28b1c17056d10ad19c6066039e6d7ed18b
Shaunmak1214 <shaonmak@gmail.com>
2021-07-17 19:35:16 +0800

    feat: Language Swticher Done
    🔥

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,53 +1,55 @@
 import MaintenanceSVG from "./svgs/Maintenance.svg";
-import UserIcon from './svgs/user.svg';
-import UserActiveIcon from './svgs/user-active.svg';
-import EmailIcon from './svgs/email.svg';
-import EmailActiveIcon from './svgs/email-active.svg';
-import WhiteFlagIcon from './svgs/white-flag.svg';
-import GoogleLoginIcon from './svgs/google-outline.svg';
-import BackButtonIcon from './svgs/back-button.svg';
-import InfoIcon from './svgs/info.svg';
-import ImageIcon from './svgs/image.svg';
-import LocationIcon from './svgs/location.svg';
-import FlagIcon from './svgs/flag.svg';
-import Logo from './svgs/logo.svg';
-import RiceIcon from './svgs/rice.svg';
-import UpvoteIcon from './svgs/upvote.svg';
-import SupportedIcon from './svgs/supported.svg';
-import PinIcon from './svgs/pin.svg';
-import GroceriesOutlineIcon from './svgs/groceries-outline.svg'
-import LogoutIcon from './svgs/logout.svg'
-import GoogleMapsIcon from './svgs/google-maps.svg';
-import SambalSosLogo from './sambalsos.svg';
-import GoogleIcon from './svgs/google.svg';
-import FoodBankIcon from './svgs/groceries.svg';
-import SirenIcon from './svgs/siren.svg';
-import PhoneIcon from './svgs/phone.svg';
+import UserIcon from "./svgs/user.svg";
+import UserActiveIcon from "./svgs/user-active.svg";
+import EmailIcon from "./svgs/email.svg";
+import EmailActiveIcon from "./svgs/email-active.svg";
+import WhiteFlagIcon from "./svgs/white-flag.svg";
+import GoogleLoginIcon from "./svgs/google-outline.svg";
+import BackButtonIcon from "./svgs/back-button.svg";
+import InfoIcon from "./svgs/info.svg";
+import ImageIcon from "./svgs/image.svg";
+import LocationIcon from "./svgs/location.svg";
+import FlagIcon from "./svgs/flag.svg";
+import Logo from "./svgs/logo.svg";
+import RiceIcon from "./svgs/rice.svg";
+import UpvoteIcon from "./svgs/upvote.svg";
+import SupportedIcon from "./svgs/supported.svg";
+import PinIcon from "./svgs/pin.svg";
+import GroceriesOutlineIcon from "./svgs/groceries-outline.svg";
+import LogoutIcon from "./svgs/logout.svg";
+import GoogleMapsIcon from "./svgs/google-maps.svg";
+import SambalSosLogo from "./sambalsos.svg";
+import GoogleIcon from "./svgs/google.svg";
+import FoodBankIcon from "./svgs/groceries.svg";
+import SirenIcon from "./svgs/siren.svg";
+import PhoneIcon from "./svgs/phone.svg";
+import LanguageIcon from "./svgs/language.svg";
 
 export {
-    MaintenanceSVG,
-    UserIcon,
-    UserActiveIcon,
-    EmailIcon,
-    EmailActiveIcon,
-    WhiteFlagIcon,
-    GoogleLoginIcon,
-    BackButtonIcon,
-    InfoIcon,
-    ImageIcon,
-    LocationIcon,
-    FlagIcon,
-    Logo,
-    RiceIcon,
-    UpvoteIcon,
-    SupportedIcon,
-    PinIcon,
-    GroceriesOutlineIcon,
-    LogoutIcon,
-    GoogleMapsIcon,
-    SambalSosLogo,
-    GoogleIcon,
-    FoodBankIcon,
-    SirenIcon,
-    PhoneIcon
-}
+  MaintenanceSVG,
+  UserIcon,
+  UserActiveIcon,
+  EmailIcon,
+  EmailActiveIcon,
+  WhiteFlagIcon,
+  GoogleLoginIcon,
+  BackButtonIcon,
+  InfoIcon,
+  ImageIcon,
+  LocationIcon,
+  FlagIcon,
+  Logo,
+  RiceIcon,
+  UpvoteIcon,
+  SupportedIcon,
+  PinIcon,
+  GroceriesOutlineIcon,
+  LogoutIcon,
+  GoogleMapsIcon,
+  SambalSosLogo,
+  GoogleIcon,
+  FoodBankIcon,
+  SirenIcon,
+  PhoneIcon,
+  LanguageIcon,
+};
---
+++ src/components/LanguageSwitcher/index.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+import { resources } from "../../i18n";
+
+import * as SSIcons from "../../assets";
+
+const Index = () => {
+  const { t, i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(
+    `${i18n.languages[0]}`
+  );
+  const [focused, setFocused] = useState(false);
+
+  const componentRef = useRef();
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+    function handleClick(e: any) {
+      if (componentRef && componentRef.current) {
+        const ref: any = componentRef.current;
+        if (ref.contains(e.target)) {
+          setFocused(true);
+        } else {
+          setFocused(false);
+        }
+      }
+    }
+  }, []);
+
+  const changeLanguage = (value) => {
+    setCurrentLanguage(value);
+    i18n.changeLanguage(value);
+  };
+
+  const LanguageChooserBlockRenderer = () => {
+    return Object.keys(resources).map((key) => {
+      return (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          borderRadius="8px"
+          w="100%"
+          backgroundColor={currentLanguage === key ? "#EAEAEA" : "#FFFFFF"}
+          padding="0.5rem"
+          mb="10px"
+          onClick={() => {
+            changeLanguage(key);
+          }}
+        >
+          <Text>{key}</Text>
+        </Flex>
+      );
+    });
+  };
+
+  // styles that make me feel better xd
+  const smoothTransition = {
+    transition:
+      "all 300ms cubic-bezier(0.740, -0.175, 0.000, 1.080) !important;",
+    "transition-timing-function":
+      "cubic-bezier(0.740, -0.175, 0.000, 1.080) !important;",
+  };
+
+  return (
+    <Flex
+      ref={componentRef}
+      borderRadius="50%"
+      padding="1rem"
+      backgroundColor="#FFFFFF"
+      border="1px solid #EAEAEA"
+      zIndex="1001"
+      position="fixed"
+      bottom="225px"
+      right="20px"
+      boxShadow="0px 8px 20px rgba(147, 147, 147, 0.25)"
+    >
+      <Image
+        src={SSIcons.LanguageIcon}
+        alt="Language Switcher"
+        height="20px"
+        width="20px"
+      />
+
+      <Flex
+        position="absolute"
+        right="0px"
+        bottom="65px"
+        padding="0.5rem"
+        borderRadius="8px"
+        border="1px solid #EAEAEA "
+        flexDir="column"
+        justifyContent="flex-start"
+        alignItems="center"
+        maxH="250px"
+        w="250%"
+        backgroundColor="#FFFFFF"
+        transformOrigin="bottom"
+        transform={focused === true ? "scaleY(1.00)" : "scaleY(0.00)"}
+        opacity={focused === true ? "1" : "0"}
+        zIndex={focused === true ? "-1" : "1001"}
+        {...smoothTransition}
+      >
+        <LanguageChooserBlockRenderer />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Index;
