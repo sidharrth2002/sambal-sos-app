dde1df992c867460dd009fd67c54449ce807c3a6
Restyled.io <commits@restyled.io>
2022-09-07 15:31:44 +0000

    Restyled by clang-format

--- packages/webapp/src/components/SocialHandles/index.js
+++ packages/webapp/src/components/SocialHandles/index.js
@@ -1,29 +1,30 @@
-import React from "react";
 import {
   Box,
   Container,
-  SimpleGrid,
-  VStack,
   Heading,
-  Text,
   HStack,
   Link,
+  SimpleGrid,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { useTranslation, Trans } from "react-i18next";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+import {Trans, useTranslation} from "react-i18next";
 
 export default function SocialHandles() {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
 
   return (
     <Box padding="3rem">
       <Container maxw="container.sm">
-        <SimpleGrid columns={{ sm: 1, md: 1 }} spacing={20} textAlign={"left"}>
+        <SimpleGrid columns={
+    { sm: 1, md: 1 }} spacing={20} textAlign={"left"}>
           <VStack spacing={5}>
             <Heading as="h2" size={"xl"}>
               {t("auth.join-our-communities")}
@@ -31,20 +32,21 @@ export default function SocialHandles() {
             <Text>
               <Trans
                 i18nKey={"auth.communities-description"}
-                components={{ bold: <strong /> }}
-              />
-            </Text>
-            <HStack spacing={10} justifyContent={"center"}>
-              <Link href="https://www.facebook.com/sambalsosapp">
-                <FontAwesomeIcon icon={faFacebook} color="blue" size="3x" />
-              </Link>
-              <Link href="https://www.instagram.com/sambal_sos/">
-                <FontAwesomeIcon icon={faInstagram} color="red" size="3x" />
-              </Link>
-              <Link href="https://twitter.com/SambalSos">
-                <FontAwesomeIcon
-                  icon={faTwitter}
-                  color="dodgerblue"
+                components={{ bold: <strong />
+}
+}
+/>
+            </Text > <HStack spacing = {10} justifyContent = {"center"}>
+    <Link href = "https://www.facebook.com/sambalsosapp">
+    <FontAwesomeIcon icon = {faFacebook} color = "blue" size = "3x" />
+    </Link>
+              <Link href="https:/ / www.instagram.com / sambal_sos /
+             ">
+         < FontAwesomeIcon icon = {faInstagram} color = "red" size = "3x" /><
+    /Link>
+              <Link href="https:/ / twitter.com / SambalSos ">
+    < FontAwesomeIcon
+icon = {faTwitter} color = "dodgerblue"
                   size="3x"
                 />
               </Link>
@@ -54,4 +56,4 @@ export default function SocialHandles() {
       </Container>
     </Box>
   );
-}
+                  }
